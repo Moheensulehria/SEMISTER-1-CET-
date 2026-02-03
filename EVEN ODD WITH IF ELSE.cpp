37200c3cb6c9da6c048d9bf9ff773fb3ea179a40
MOHEEN SULEHRIA <moheensulehria@gmail.com>
2026-02-03 18:34:02 +0500

    Add files via upload

---
+++ EVEN ODD WITH IF ELSE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void checkEvenOdd(int num)
+{
+    if (num % 2 == 0)
+	{
+        cout << num << " is Even" << endl;
+    }
+	else
+	{
+        cout << num << " is Odd" << endl;
+    }
+}
+
+int main()
+{
+    checkEvenOdd(10);
+    checkEvenOdd(7);
+    checkEvenOdd(25);
+
+    return 0;
+}
+
---
+++ VOID GREETING.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void welcomeMessage()
+{
+    cout << "Welcome to C++ Void Functions!" << endl;
+}
+
+int main()
+{
+    welcomeMessage();
+
+    return 0;
+}
+
