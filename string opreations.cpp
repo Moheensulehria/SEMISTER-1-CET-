31c1a15828af80987675837196d77e09c3dbb98a
MOHEEN SULEHRIA <moheensulehria@gmail.com>
2026-02-03 18:10:14 +0500

    Add files via upload

---
+++ string opreations.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"she said,""let's learn c++!"<<endl;
+	cout<<"to start,save your file in local drives."<<endl;
+	cout<<"good luck!"<<endl;
+	return 0;
+}
